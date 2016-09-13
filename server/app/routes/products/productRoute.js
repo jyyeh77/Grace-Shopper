d676ae66e8ce32889fa0e1b489487412709bf52b
“Joshua <j.maas.howard@gmail.com>
2016-09-13 16:56:19 -0400

    product routes/instance method

--- server/app/routes/products/productRoute.js
+++ server/app/routes/products/productRoute.js
@@ -4,3 +4,45 @@ var Product = require('../../../db/models/product');
 
 module.exports = router;
 
+
+
+// retrieves single product if ID parameter is specified in an /api/products/:id route
+router.param('id', function (req, res, next, id) {
+  Product.findById(id)
+    .then(foundProduct => {
+      if (!foundProduct) res.sendStatus(404)
+      else req.requestedProduct = foundProduct;
+      next();
+    })
+    .catch(next);
+})
+
+// fetch one product instance
+router.get('/:id', function (req, res, next) {
+  res.send(req.requestedProduct);
+})
+
+// fetch similar
+router.get('/:id/similar', function(req, res, next) {
+	req.requestedProduct.getSimilar()
+		.then(res.send)
+		.catch(next);
+}
+
+
+// admins can update current product info here
+router.put('/:id', function (req, res, next) {
+  req.requestedProduct.update(req.body)
+    .then(product => {
+      res.send(product);
+    })
+    .catch(next)
+})
+
+router.delete('/:id', function (req, res, next) {
+  req.requestedProduct.destroy()
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(next);
+})
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,32 +1,63 @@
 'use strict';
 
 var Sequelize = require('sequelize');
+var _ = require('lodash/array');
 
 var db = require('../_db');
 
-module.exports = db.define('product', {
-  title: {
-    type: Sequelize.STRING
-  },
-  description: {
-    type: Sequelize.STRING
-  },
-  specs: {
-    type: Sequelize.JSONB
-  },
-  price: {
-    type: Sequelize.INTEGER
-  },
-  quantity: {
-    type: Sequelize.INTEGER
-  },
-  creationDate: {
-    type: Sequelize.STRING
-  },
-  imageUrl: {
-    type: Sequelize.STRING
-  }
+var Category = require('./category');
+
+var Product = db.define('product', {
+    title: {
+        type: Sequelize.STRING
+    },
+    description: {
+        type: Sequelize.STRING
+    },
+    specs: {
+        type: Sequelize.JSONB
+    },
+    price: {
+        type: Sequelize.INTEGER
+    },
+    quantity: {
+        type: Sequelize.INTEGER
+    },
+    creationDate: {
+        type: Sequelize.STRING
+    },
+    imageUrl: {
+        type: Sequelize.STRING
+    }
+}, {
+    instanceMethods: {
+        getSimilar: function() {
+            return this.getCategories()
+                .then(function(productCategories) {
+                    return Promise.all(
+                        productCategories.map(function(category) {
+                            return category.getProducts({
+                                where: {
+                                    id: {
+                                        $ne: this.id
+                                    }
+                                }
+                            });
+                        })
+                    );
+                })
+                .then(function(similarProducts) {
+                    var output = [];
+                    flattenedProducts = _.flatten(similarProducts);
+                    flattenedProducts.forEach(function(prod) {
+                        if (!output.includes(prod)) output.push(prod);
+                    });
+                    return output;
+                });
+        }
+    }
 });
 
 
 
+module.exports = Product;
